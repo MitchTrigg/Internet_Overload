f197ad209490d0ce559bbf11d5c748ed0a58bcdc
Mitch Trigg <mitrigg@davidson.edu>
2022-04-10 15:11:14 -0400

    4-10

--- Assets/Scripts/LevelSceneScript.cs
+++ Assets/Scripts/LevelSceneScript.cs
@@ -7,11 +7,15 @@ public class LevelSceneScript : MonoBehaviour
 {
     public string firstLevel;
     public string secondLevel;
+    public string thirdLevel;
 
     public void level1(){
         SceneManager.LoadScene(firstLevel);
     }
     public void level2(){
         SceneManager.LoadScene(secondLevel);
+    }
+    public void level3(){
+        SceneManager.LoadScene(thirdLevel);
     }
 }
